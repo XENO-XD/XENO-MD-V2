dc5d7568e85641d82db8ab38b648a67a5be5120f
XENO-SIR-MD <vincenzoexe6@gmail.com>
2026-02-06 12:19:23 +0530

    config: update bot pairing number to 919645991937

--- config.js
+++ config.js
@@ -9,6 +9,6 @@ module.exports = {
     ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/jhdz71.jpeg",
     ALIVE_MSG: process.env.ALIVE_MSG || "*🤖𝐇𝐞𝐲 𝐈'𝐦 💃xᴇɴᴏ ᴍᴅ 🤍 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐁𝐨𝐭⚡*\n\n*🔔𝐈'𝐦 𝐀𝐥𝐢𝐯𝐞 𝐍𝐨𝐰🎠*\n\n*⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : xᴇɴᴏ ᴍᴅ",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
-    BOT_NUMBER: process.env.BOT_NUMBER || "918136810956",
+    BOT_NUMBER: process.env.BOT_NUMBER || "919645991937",
     PREFIX: process.env.PREFIX || ",",
 };
